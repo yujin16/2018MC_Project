f20828f5dd79231bc3c5cbe1347c042527b80bb2
ByoungWook Park <qkrquddnr99@gmail.com>
2018-05-26 14:24:23 +0900

    Add diagramsequence~

---
+++ SelfInterior/si_pch.h
@@ -0,0 +1,21 @@
+#ifndef SI_PCH_H
+#define SI_PCH_H
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QDesktopWidget>
+#include <QMouseEvent>
+#include <QPixmap>
+#include <QWidget>
+#include <QtCore>
+#include <QtGui>
+#include <QtWidgets>
+#include <QDebug>
+
+#include "screenshot.h"
+#include "ui_screenshot.h"
+
+#include "ui_mainwindow.h"
+#include "mainwindow.h"
+
+#endif // SI_PCH_H
